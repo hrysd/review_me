05dde456cd2b4743f9f9ca5f266a2ddcc3e42938
Hiroshi Yoshida <hrysd22@gmail.com>
2017-03-02 20:46:17 +0900

    Initialize commit

---
+++ functions/review_me/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+import axios    from 'axios';
+import lambda   from 'apex.js';
+import octonode from 'octonode';
+
+const GITHUB_TOKEN     = process.env.GITHUB_TOKEN;
+const IDOBATA_HOOK_URL = process.env.IDOBATA_HOOK_URL;
+const REPOSITORIES     = process.env.REPOSITORIES;
+const TARGET_RABELS    = process.env.TARGET_RABELS;
+
+const labels = TARGET_RABELS.split(',');
+const client = octonode.client(GITHUB_TOKEN);
+
+const repositories = REPOSITORIES.split(',').map((repository) => {
+  return labeledIssues(client.repo(repository), labels);
+});
+
+function labeledIssues(repo, labels) {
+  return new Promise((resolve, reject) => {
+    const a = repo.issues({labels: labels, state: 'open'}, function(err, issues) {
+      let text = `### ${repo.name}\n\n`;
+
+      if (err) { return 'Fail to get PRs'; }
+
+      text += issues.map((issue) => {
+        return `- [${[issue.title]}](${issue.html_url})`;
+      }).join('\n');
+
+      resolve(text);
+    });
+  });
+}
+
+export default lambda((e, ctx) => {
+  return new Promise((resolve, reject) => {
+    Promise.all(repositories).then((data) => {
+      let text = '## Let\'s review \n\n';
+
+      text += data.join('\n\n');
+
+      axios.post(
+        IDOBATA_HOOK_URL,
+        {
+          source: text,
+          format: 'markdown'
+        }
+      ).then(() => {
+        resolve('success');
+      }).catch(() => {
+        reject('Fail to post');
+      });
+    });
+  });
+});
